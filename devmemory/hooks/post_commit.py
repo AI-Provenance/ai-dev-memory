5be9f1d7c97a83e1aaed3c5cc6b0f3d8126d594c
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2026-02-23 15:40:51 +0100

    fix: remove unknown command

--- devmemory/hooks/post_commit.py
+++ devmemory/hooks/post_commit.py
@@ -4,10 +4,3 @@ HOOK_TEMPLATE = """#!/bin/bash
 (sleep 2 && devmemory sync --latest 2>/dev/null) &
 # <<< devmemory post-commit hook <<<
 """
-
-POST_CHECKOUT_HOOK_TEMPLATE = """#!/bin/bash
-
-# >>> devmemory post-checkout hook >>>
-(sleep 2 && devmemory refresh 2>/dev/null) &
-# <<< devmemory post-checkout hook <<<
-"""
